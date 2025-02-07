4408cc0418c61c22a63d82f64efb0757a868d3b1
Nagi Mukuchi <bakachan9x@gmail.com>
2025-02-07 21:24:42 +0700

    add bài tap 6 ly thuyet

---
+++ DemoPreIT/LyThuyet/bai tap/Ly thuyet buoi 6/Ly Thuyet buoi 6/S12/S12.cpp
@@ -0,0 +1,20 @@
+// S12.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Nhap hai so a : ";
+    cin >> a ;
+
+    cout << "Nhap hai so b : ";
+    cin >> b;
+
+    
+}
